45c7acee422603ceb7c15bac211eb64c7867d998
Suraj Yadav <sy083609@gmail.com>
2025-05-03 10:15:04 +0530

    updated commit

---
+++ numeric_object.py
@@ -0,0 +1,39 @@
+x=int()
+print(x)
+print(type(x))
+
+y=float()
+print(y)
+print(type(y))
+
+z=complex()
+print(z)
+print(type(z))
+
+a=0
+print(a)
+print(type(a))
+
+x=input("Enter any no")
+print(x)
+print(type(x))
+
+x=int (input("enter number"))
+print(x)
+print(type(x))
+
+x=eval (input("enter anything:"))
+print(x)
+print(type(x))
+
+l=[3,4,5,6,7,8,'p']
+
+print(max(l))
+print(min(l))
+print(len(l))
+print(sum(l))
+
+a=9
+b=8
+add=a+b
+print(add)
