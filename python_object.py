fd7d525186c79b24a0186605367f7f4f32173624
Suraj Yadav <sy083609@gmail.com>
2025-05-04 08:47:30 +0530

    updated commit

---
+++ python_object.py
@@ -0,0 +1,15 @@
+x=10
+y=20
+print(id(x),id(y))
+
+x='python'
+y='python'
+print(id(x),id(y))
+
+x=[1,2,3]
+y=[1,2,3]
+print(id(x),id(y))
+
+x=(1,2,3)
+y=(1,2,3)
+print(id(x),id(y))
---
+++ space_required.py
@@ -0,0 +1,17 @@
+import sys
+x=int()
+y=str()
+z=list()
+p=tuple()
+q=dict()
+r=set()
+s=frozenset()
+
+
+print("Required Bit",sys.getsizeof(x))
+print("Required Bit",sys.getsizeof(y))
+print("Required Bit",sys.getsizeof(z))
+print("Required Bit",sys.getsizeof(p))
+print("Required Bit",sys.getsizeof(q))
+print("Required Bit",sys.getsizeof(r))
+print("Required Bit",sys.getsizeof(s))
