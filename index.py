bc72e2011a637a61844a7ebd990eb02a6a0a473e
Suraj Yadav <sy083609@gmail.com>
2025-05-06 23:10:15 +0530

    updated commit

---
+++ index.py
@@ -0,0 +1,4 @@
+s='python'
+print(s.index('o'))
+print(s.index('y',2))
+print(s.index(('h',1,3)))
---
+++ slice.py
@@ -0,0 +1,9 @@
+s='I LOVE PYTHON'
+print(s[::])
+print(s[::2])
+print(s[::-1])
+print(s[2:10])
+print(s[7:-10])
+print(s[7:-2])
+print(s[7:7])
+print(s[::-1][1:7][::-1][2:3])
