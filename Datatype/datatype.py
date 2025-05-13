26603d6667ddb95f749ea500d21f03eb93fbe428
Suraj Yadav <sy083609@gmail.com>
2025-05-13 17:38:58 +0530

    updated commit

---
+++ Datatype/datatype.py
@@ -0,0 +1,7 @@
+s='python'
+print(s.isascii())
+print(s.isdigit())
+print(s.isspace())
+print(s.isnumeric())
+print(s.isdecimal())
+print(s.isupper())
---
+++ Datatype/list_python.py
@@ -0,0 +1,10 @@
+l1=['python','java','php']
+l2=[10,20,30]
+print(" Max in array:",max(l1))
+print(" max in array:",max(l2))
+print(min(l1))
+print(min(l2))
+print(sum(l2))
+print(len(l1))
+print(len(l2))
+
