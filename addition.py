e436ad42d9b6d08e52ea51c582cde5667871530a
Suraj <sy083609@gmail.com>
2026-03-01 15:34:46 +0530

    updated commit

--- addition.py
+++ addition.py
@@ -1,22 +1,26 @@
-a=3
-b=9
-sum=a+b
-print("The sum is:",sum)
+# a=3
+# b=9
+# sum=a+b
+# print("The sum is:",sum)
 
-x=9
-y=4
-subs=x-y
-print("The substraction is :",subs)
-
-
-c=9
-d=8
-sum=c+d
-print(" Sum is :",sum)
+# x=9
+# y=4
+# subs=x-y
+# print("The substraction is :",subs)
 
 
-print(''' 
-1.hello
-2.hii
+# c=9
+# d=8
+# sum=c+d
+# print(" Sum is :",sum)
 
-''')
+
+# print(''' 
+# 1.hello
+# 2.hii
+
+# ''')
+
+x=[3,4]
+y=x
+print(x is y)
