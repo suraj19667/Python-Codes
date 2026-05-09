06a50f0ae734861d1a20126e8be3c992bca08d2a
Suraj <sy083609@gmail.com>
2026-05-09 17:08:37 +0530

    updated commit

--- assinged_operatior.py
+++ assinged_operatior.py
@@ -11,22 +11,22 @@ print(z)
 x=y=z=6+6j
 print(x,y,z)'''
 
-# x=10.5+2j
-# y=20.2+3j
-# z1=x+y
-# print(type(z1))
-# z2=x-y
-# print(type(z2))
-# z3=x*y
-# print(type(z3))
-# z4=x/y
-# print(type(z4))
-# z5=x//y
-# print(type(z5))
-# # z6=x%y
-# print(type(z6))
-# z7=x**y
-# print(type(z7))
+x=10.5+2j
+y=20.2+3j
+z1=x+y
+print(type(z1))
+z2=x-y
+print(type(z2))
+z3=x*y
+print(type(z3))
+z4=x/y
+print(type(z4))
+z5=x//y
+print(type(z5))
+# z6=x%y
+print(type(z6))
+z7=x**y
+print(type(z7))
 
 
 a=10
