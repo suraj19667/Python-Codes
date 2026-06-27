163e70e72af2b5df03748844a7f9da9016018989
Suraj <sy083609@gmail.com>
2026-06-27 23:35:57 +0530

    updated commit

--- Loop/for.py
+++ Loop/for.py
@@ -5,18 +5,18 @@
 #     l1.append(i**2)
 # print(l1)
 
-#user input (a,b,c,d,e)=>(e,f,g,h,i)
+user input (a,b,c,d,e)=>(e,f,g,h,i)
 
-# x='a'
-# print(ord('a'))
-# print(ord('a')+4)
+x='a'
+print(ord('a'))
+print(ord('a')+4)
 
-# print(chr(ord('a')+4))
+print(chr(ord('a')+4))
 
-# s=input("enter string:")
-# s1=()
-# for i in s:
-#     print(chr(ord(i)+4))
+s=input("enter string:")
+s1=()
+for i in s:
+    print(chr(ord(i)+4))
 
 
 # s='abcde'
